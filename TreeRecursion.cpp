706287715d9b1fcc8ef51a0673a1f472f0e40269
fazlerabbi13 <mohammad0132794@gmail.com>
2025-08-29 21:50:59 +0600

    recursive call.

---
+++ TreeRecursion.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void tree(int n) {
+    if (n == 0)
+        return;
+    cout << n << " ";
+
+    // Two recursive calls
+    // tree(n - 1);
+    // tree(n - 1);
+}
+
+int main() {
+    tree(3);
+    return 0;
+}
