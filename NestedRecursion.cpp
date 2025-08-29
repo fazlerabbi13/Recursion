fc684c80755324b515f11ec42fe3b80c89723fa0
fazlerabbi13 <mohammad0132794@gmail.com>
2025-08-29 21:56:55 +0600

    base case called.

---
+++ NestedRecursion.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int nested(int n) {
+    if (n > 100)
+        return n - 10;
+
+    // Recursive call inside another recursive call
+    return nested(nested(n + 11));
+}
+
+int main() {
+    // cout << nested(95);
+    return 0;
+}
