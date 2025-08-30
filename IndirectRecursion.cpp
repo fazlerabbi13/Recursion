7476f71f8eee79ff2123f44af82885534a49588b
fazlerabbi13 <mohammad0132794@gmail.com>
2025-08-30 16:15:59 +0600

    create a function name funcA().

---
+++ IndirectRecursion.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void funcA(int);
+// void funcB(int);
+
+void funcA(int n) {
+    if (n > 0) {
+        cout << n << " ";
+        funcB(n - 1);
+    }
+}
+
+// void funcB(int n) {
+//     if (n > 0) {
+//         cout << n << " ";
+//         funcA(n / 2);
+//     }
+// }
+
+int main() {
+    funcA(10);
+    return 0;
+}
