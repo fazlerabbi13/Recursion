93f24e9d4f95f8411fd9327906d14c4644d210e5
fazlerabbi13 <mohammad0132794@gmail.com>
2025-09-28 10:24:55 +0600

    print n times 'I love Recursion'  using recursion.

--- propose.cpp
+++ propose.cpp
@@ -1,18 +1,18 @@
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// void printRecursion(int n) {
+void printRecursion(int n) {
    
-//     if (n == 0)
-//         return;
+    if (n == 0)
+        return;
 
-//     cout << "I love Recursion" << endl;
-//     printRecursion(n - 1);
-// }
+    cout << "I love Recursion" << endl;
+    printRecursion(n - 1);
+}
 
-// int main() {
-//     int N;
-//     cin >> N;       
-//     printRecursion(N);
-//     return 0;
-// }
+int main() {
+    int N;
+    cin >> N;       
+    printRecursion(N);
+    return 0;
+}
